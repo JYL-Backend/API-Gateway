37f5493cba52428b6cd1b10be73ead0ec1ad147e
JYL-Backend <dustmqdyd1213@gmail.com>
2021-08-24 23:41:23 +0900

    route request header ,response header

--- First-Service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ First-Service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,14 +1,24 @@
 package com.example.firstservice.controller;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Log4j2
 public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcom to the first Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+         log.info(header);
+         return "Hello World in First Service";
     }
 }
--- Second-Service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
+++ Second-Service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
@@ -1,15 +1,24 @@
 package com.example.secondservice.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/second-service")
+@Log4j2
 public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcom to the second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+        log.info(header);
+        return "Hello World in second Service";
     }
 }
